d71a443b5fe19f8c24e1ad821ac07e5845fe3845
suqing <874717373@qq.com>
2018-12-06 17:11:39 +0800

    update wsmud

--- wsmud/autoPractise/autoPractise.js
+++ wsmud/autoPractise/autoPractise.js
@@ -1,160 +1,149 @@
-var data = new Array();
+var array_url = ["基础技能", "峨眉派技能", "逍遥派技能", "其他"];
 
 function start() {
-	$.ajax({
-		url: 'skillIdData',
-		dataType: 'text',
-	}).done(analyseData);
-	lastButton();
-}
-
-function analyseData(dataFile) {
-	var dataArray1 = dataFile.split(/\r?\n|\r/); // 按照行分割数据
-	for (var i = 0; i < dataArray1.length; i++) {
-		var  dataArray2 = dataArray1[i].split(','); // 按照逗号分割数据
-		data[i] = dataArray2;
+	for (var i = 0; i < array_url.length; i++) {
+		getDataFromURL(array_url[i]);
 	}
-	createButton(data);
+	createPractiseCodeButton();
+	creatBackToVillaCodeButton();
 }
 
-function createButton(array) {
-	for (var i = 0; i < array.length; i++) {
-		var skillName = document.createTextNode(array[i][0]); // 技能名称
-		var skillCode = document.createTextNode(array[i][1]); // 技能代码
+
+function getDataFromURL(data_url) {
+	$.ajax({
+ 		url: data_url,
+ 		dataType: 'text',
+ 	}).done(function(dataFile) {
+ 		if (!dataFile) return;
+ 		var dataArray = new Array();
+ 		var dataArray1 = dataFile.split(/\r?\n|\r/);
+ 		for (var i = 0; i < dataArray1.length; i++) {
+ 			var  dataArray2 = dataArray1[i].split(',');
+ 			dataArray[i] = dataArray2;
+ 		}
+ 		createSkillButton(dataArray, data_url);
+ 	});
+}
+
+function createSkillButton(dataArray, ID) {
+	var skills = document.getElementById("skills");
+	var div = document.createElement("div");
+	div.id = ID;
+ 	var p = document.createElement("p");
+ 	p.innerHTML = ID;
+ 	div.appendChild(p);
+ 	skills.appendChild(div);
+	for (var i = 0; i < dataArray.length; i++) {
 		var button = document.createElement("button");
-		button.id = array[i][1];
 		button.type = "button";
-		button.className = "button";
-		button.onclick = skillButton;
-		button.appendChild(skillName);
-		var div = document.getElementById("list");
+		button.innerHTML = dataArray[i][0]; // 技能名称
+		button.value = dataArray[i][1]; // 技能系数
+		button.id = dataArray[i][2]; // 技能代码
+		// 如果没有技能代码的话
+		if (!dataArray[i][2]) {
+			button.className = "skillButtonClicked";
+			button.onclick = null;
+		} else {
+			button.className = "skillButton"; // 配置CSS
+			button.onclick = clickSkillButton;
+		}
+		var div = document.getElementById(ID);
 		div.appendChild(button);
 	}
 }
-
-function skillButton() {
-	addSkill(this.innerHTML, this.id);
+function clickSkillButton() {
+	addPractiseSkill(this.innerHTML, this.value, this.id);
 	this.onclick = null;
-	this.style.color = "white";
-	this.style.border = "1px solid white";
-}
+	this.className = "skillButtonClicked"; // 配置CSS 被点击后
+};
 
-function addSkill(skillName, skillCode) {
-	var table = document.getElementById("table");
+function addPractiseSkill(skillName, skillValue, skillCode) {
+	var table = document.getElementById("table"); // 表格
 	var tr = document.createElement("tr"); // 行
-	tr.id = skillCode + "tr";
-	tr.appendChild(td1(skillCode));
-	tr.appendChild(td2(skillCode, skillName));
-	tr.appendChild(td34(skillCode, "3"));
-	tr.appendChild(td34(skillCode, "4"));
-	tr.appendChild(td5(skillCode));
-	tr.appendChild(td6(skillCode));
-
 	table.appendChild(tr);
+	tr.id = skillCode + "tr";
+	for (var i = 0; i < 6; i++) tr.appendChild(set_TD(i, skillName, skillValue, skillCode));
 }
 
-function td1(skillCode) {
+function set_TD(index, skillName, skillValue, skillCode) {
 	var td = document.createElement("td");
-	td.id = skillCode + "1";
-	var selectColor = document.createElement("select");
-	selectColor.id = skillCode + "se";
-	selectColor.options.add(new Option("白色", "2.5"));
-	selectColor.options.add(new Option("绿色", "5"));
-	selectColor.options.add(new Option("蓝色", "7.5"));
-	selectColor.options.add(new Option("黄色", "10"));
-	selectColor.options.add(new Option("紫色", "12.5"));
-	selectColor.options.add(new Option("橙色", "15"));
-	selectColor.onchange = function() {
-		var value = this.options[this.selectedIndex].value;
-		var skillCode = this.id.substring(0, this.id.length - 2);
-		var tr = document.getElementById(skillCode + "tr");
-		switch(value) {
-			case "2.5":
-				tr.style.color = "white";break;
-			case "5":
-				tr.style.color = "#33CC33";break;
-			case "7.5":
-				tr.style.color = "aqua";break;
-			case "10":
-				tr.style.color = "yellow";break;
-			case "12.5":
-				tr.style.color = "#CC00FF";break;
-			case "15":
-				tr.style.color = "orange";break;
-		}
-	};
-	td.appendChild(selectColor);
+	td.id = skillCode + index;
+	var tr = document.getElementById(skillCode + "tr");
+	tr.value = skillValue;
+	if (index == 0) {
+		var selectColor = document.createElement("select");
+		selectColor.id = skillCode + index;
+		selectColor.options.add(new Option("白色", "1"));
+		selectColor.options.add(new Option("绿色", "2"));
+		selectColor.options.add(new Option("蓝色", "3"));
+		selectColor.options.add(new Option("黄色", "4"));
+		selectColor.options.add(new Option("紫色", "5"));
+		selectColor.options.add(new Option("橙色", "6"));
+		selectColor.onchange = function() {
+			var skillValue = this.selectedIndex + 1;
+			var skillCode = this.id.substring(0, this.id.length - 1);
+			var tr = document.getElementById(skillCode + "tr");
+			tr.className = "tr" + skillValue;
+			tr.value = skillValue;
+			updateData(skillCode);
+		};
+		selectColor.selectedIndex = skillValue - 1;
+		tr.className = "tr" + skillValue;
+		td.appendChild(selectColor);
+	}
+	if (index == 1) td.innerHTML = skillName;
+	if (index == 2 || index == 3) {
+		if (index == 2) td.id = skillCode + "level1";
+		if (index == 3) td.id = skillCode + "level2";
+		td.innerHTML = 0;
+		td.contentEditable = "true";
+		td.style.background = "#333333";
+		td.oninput = function() {
+			updateData(this.id.substring(0, this.id.length - 6));
+		};
+	}
+	if (index == 4) td.innerHTML = 0;
+	if (index == 5) {
+		td.style.width = "25px";
+		var button = document.createElement(button);
+		td.appendChild(button);
+		button.type = "button";
+		button.innerHTML = "×";
+		button.onclick = function() {
+			var tr = document.getElementById(skillCode + "tr");
+			var table = document.getElementById("table");
+			table.removeChild(tr);
+			var button = document.getElementById(skillCode);
+			button.className = "skillButton";
+			button.onclick = clickSkillButton;
+		};
+	}
 	return td;
 }
 
-function td2(skillCode, skillName) {
-	var td = document.createElement("td"); 
-	td.id = skillCode + "2";
-	td.innerHTML = skillName;
-	return td;
-}
-
-function td34(skillCode, count) {
-	var td = document.createElement("td");
-	td.id = skillCode + count;
-	td.innerHTML = "0";
-	td.contentEditable = "true";
-	td.style.background = "#333333";
-	td.oninput = update;
-	return td;
-}
-
-function td5(skillCode) {
-	var td = document.createElement("td");
-	td.id = skillCode + "5";
-	td.innerHTML = "0";
-	return td;
-}
-
-function td6(skillCode) {
-	var td = document.createElement("td");
-	td.id = skillCode + "5";
-	td.style.width = "25px";
-	var button = document.createElement(button);
-	button.innerHTML = "×";
-	button.type = "button";
-	button.onclick = function() {
-		var tr = document.getElementById(skillCode + "tr");
-		var table = document.getElementById("table");
-		table.removeChild(tr);
-		var backButton = document.getElementById(skillCode);
-		backButton.style.color = "gray";
-		backButton.style.border = "1px solid gray";
-		backButton.onclick = skillButton;
-	};
-	td.appendChild(button);
-	return td;
-}
-
-function update() {
-	var skillCode = this.id.substring(0, this.id.length - 1);
-	var select = document.getElementById(skillCode + "se");
-	var k = parseFloat(select.options[select.selectedIndex].value);
-	var x = parseFloat(document.getElementById(skillCode + "3").innerHTML);
-	var y = parseFloat(document.getElementById(skillCode + "4").innerHTML);
+// 改变颜色或者改变等级数据的时候，都需要更新技能数据。
+function updateData(skillCode) {
+	var tr = document.getElementById(skillCode + "tr");
+	var k = parseFloat(tr.value * 2.5);
+	var x = parseFloat(document.getElementById(skillCode + "level1").innerHTML);
+	var y = parseFloat(document.getElementById(skillCode + "level2").innerHTML);
 	var result = k * (y * y - x * x);
-	document.getElementById(skillCode + "5").innerHTML = result;
+	document.getElementById(skillCode + "4").innerHTML = result;
 }
 
 
-function lastButton() {
+function createPractiseCodeButton() {
 	var button = document.createElement("button");
-	button.innerHTML = "生成代码";
-	button.id = "lastButton";
 	button.type = "button";
-	button.onclick = clickLastButton;
+	button.id = "createPractiseCode";
+	button.innerHTML = "生成代码";
+	button.onclick = clickCreatePractiseCodeButton;
 	var p = document.getElementById("button");
 	p.appendChild(button);
 }
 
-function clickLastButton() {
-	var table = document.getElementById("table");
-	var trArray = table.children;
+function clickCreatePractiseCodeButton() {
+	var trArray = document.getElementById("table").children;
 	var codeString = "";
 	var qianneng = 0;
 	for (var i = 1; i < trArray.length; i++) {
@@ -170,4 +159,27 @@ function clickLastButton() {
 	var ps = document.getElementById("ps");
 	ps.innerHTML = "感谢测试！将以上代码复制粘贴到游戏设置框内即可！<br>" + 
 	  "请确保你有 " + qianneng + " 的潜能！以防挂机。";
+
+	var button = document.getElementById("backToVillaCodeButton");
+	button.onclick = clickBackToVillaCode;
+	button.innerHTML = "添加自动回豪宅的代码";
 }
+
+// 关于回豪宅的代码
+function creatBackToVillaCodeButton() {
+	var button = document.createElement("button");
+	button.id = "backToVillaCodeButton";
+	button.type = "button";
+	button.innerHTML = "添加自动回豪宅的代码";
+	button.onclick = clickBackToVillaCode;
+	var p = document.getElementById("button");
+	p.appendChild(button);
+}
+function clickBackToVillaCode() {
+	var code = "jh fam 0 start,go west,go west,go north,go enter,go west,";
+	var div = document.getElementById("code");
+	div.innerHTML = code + div.innerHTML;
+	this.innerHTML = "取消自动回豪宅的代码";
+	this.onclick = clickCreatePractiseCodeButton;
+}
+// END
