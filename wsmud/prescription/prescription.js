d0bfe92651d91c8b8ff144f523dc7f256b111346
suqing <874717373@qq.com>
2018-12-14 16:28:41 +0800

    update

---
+++ wsmud/prescription/prescription.js
@@ -0,0 +1,13 @@
+// Elixir 丹药
+function Elixir(name, color) {
+	this.name = name;
+	this.color = color;
+	this.effect = effect;
+	this.yaofang = yaofang;
+}
+
+
+function YuanLiao(name, color) {
+	this.name = name;
+	this.color = color;
+}
